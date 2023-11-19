7f6cab02570b63a99261d6844ce333ad13cb6e4d
Islam-cod <islamra135@gmail.com>
2023-11-19 20:21:56 +0000

    fitness excercieses V1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import ExerciseDetail from "./pages/ExerciseDetail"
+
+import Navbar from "./components/Navbar"
+
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
